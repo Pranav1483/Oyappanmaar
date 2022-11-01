2d528e1f8f2290901e64fd430e8c27a05fec9aa9
Pranav1483 <pranavp1483@gmail.com>
2022-11-01 19:49:56 +0530

    Initial Commit

--- Oyappanmaar/views.py
+++ Oyappanmaar/views.py
@@ -9,24 +9,24 @@ def homepage(request):
 
 def check(request):
     text = 'Welcome'
-    # username = request.POST.get('user', False)
-    # password = request.POST.get('passkey', False)
-    # if not username:
-    #     text = 'Please Enter Username'
-    # else:
-    #     flag = 0
-    #     creds = Credentials.objects.all().values()
-    #     for x in creds:
-    #         if username == x['username']:
-    #             flag = 1
-    #             if password == x['password']:
-    #                 text = 'Welcome ' + x['name']
-    #                 flag = 2
-    #             break
-    #     if flag == 0:
-    #         text = 'No User Found'
-    #     elif flag == 1:
-    #         text = 'Wrong Password'
+    username = request.POST.get('user', False)
+    password = request.POST.get('passkey', False)
+    if not username:
+        text = 'Please Enter Username'
+    else:
+        flag = 0
+        creds = Credentials.objects.all().values()
+        for x in creds:
+            if username == x['username']:
+                flag = 1
+                if password == x['password']:
+                     text = 'Welcome ' + x['name']
+                     flag = 2
+                break
+        if flag == 0:
+            text = 'No User Found'
+        elif flag == 1:
+            text = 'Wrong Password'
     template = loader.get_template('dashboard.html')
     context = {
         'text': text
